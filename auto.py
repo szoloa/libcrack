a6f107d9cadd5791d2f156c04431b1c6f815343c
szoloa <szoloa@hotmail.com>
2023-02-22 13:36:44 +0800

    modified:   analysis.py 	modified:   auto.py 	deleted:    data/txt/02_20_12_05.txt 	new file:   data/txt/txt.7z

--- auto.py
+++ auto.py
@@ -7,8 +7,8 @@ while True:
     try:
         main.getData()
     except:
-        print(time_url.getDay__,time_url.getTime,'is something error')
+        print(time_url.getDay__(),time_url.getTime(),'is something error')
         with open('./data/txt/{}.txt'.format(time_url.getDay__()), 'w', encoding='utf-16') as f:
-            f.write(time_url.getDay__ + time_url.getTime + 'is something error')
+            f.write(time_url.getDay__() + time_url.getTime() + 'is something error')
             print('文件已成功写入', f.name)
-    time.sleep(600000)
+    time.sleep(60)
